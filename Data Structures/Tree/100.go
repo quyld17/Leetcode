eab28ff5f8ce64f0e2d822e813ecdf1f9050e1e9
quyld17 <quyld17@gmail.com>
2023-05-10 16:46:26 +0700

    finished problem 100

---
+++ Data Structures/Tree/100.go
@@ -0,0 +1,25 @@
+// 100. Same Tree
+
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p != nil && q != nil {
+        return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+    } else {
+        return p == q
+    }
+}
